90e2be0684c8038b91ec5de6166e733ff70ad5c1
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com.>
2024-10-30 20:58:36 -0500

    Letter or number

--- main.py
+++ main.py
@@ -1,75 +1,25 @@
-#Exercise 5. Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
+#Exercise 6. Escriba un programa que determine si un caracter ingresado es letra, número, o ninguno de los dos. 
+# En caso que sea letra, determine si es mayúscula o minúscula.
 
-#Ingrese numero: 51
-#Ingrese numero: 24
-#24 51
-#
-# A continuación, escriba otro programa que haga lo mismo con tres números:
-#
-#Ingrese numero: 8
-#Ingrese numero: 1
-#Ingrese numero: 4
-#1 4 8
-#
-# Finalmente, escriba un tercer programa que ordene cuatro números:
+#Ingrese caracter: 9
+#Es numero.
 
-#Ingrese numero: 7
-#Ingrese numero: 0
-#Ingrese numero: 6
-#Ingrese numero: 1
-#0 1 6 7
+#Ingrese caracter: A
+#Es letra mayúscula.
 
-#Recuerde que su programa debe entregar la solución correcta para cualquier combinación de números, no sólo para los ejemplos mostrados aquí.
-#Hay más de una manera de resolver cada ejercicio.
+#Ingrese caracter: f
+#Es letra minúscula.
 
-number1 = int(input("Enter number: "))
-number2 = int(input("Enter number: "))
-number3 = int(input("Enter number: "))
-number4 = int(input("Enter number: "))
+#Ingrese caracter: #
+#No es letra ni número.
 
-if number1 <= number2 and number1 <= number3 and number1 <= number4:
-    minor = number1
-    if number2 <= number3 and number2 <= number4:
-        middle1, middle2 = number2, min(number3, number4)
-        major = max(number3, number4)
-    elif number3 <= number2 and number3 <= number4:
-        middle1, middle2 = number3, min(number2, number4)
-        major = max(number2, number4)
-    else:
-        middle1, middle2 = number4, min(number2, number3)
-        major = max(number2, number3)
-elif number2 <= number1 and number2 <= number3 and number2 <= number4:
-    minor = number2
-    if number1 <= number3 and number1 <= number4:
-        middle1, middle2 = number1, min(number3, number4)
-        major = max(number3, number4)
-    elif number3 <= number1 and number3 <= number4:
-        middle1, middle2 = number3, min(number1, number4)
-        major = max(number1, number4)
-    else:
-        middle1, middle2 = number4, min(number1, number3)
-        major = max(number1, number3)
-elif number3 <= number1 and number3 <= number2 and number3 <= number4:
-    minor = number3
-    if number1 <= number2 and number1 <= number4:
-        middle1, middle2 = number1, min(number2, number4)
-        major = max(number2, number4)
-    elif number2 <= number1 and number2 <= number4:
-        middle1, middle2 = number2, min(number1, number4)
-        major = max(number1, number4)
-    else:
-        middle1, middle2 = number4, min(number1, number2)
-        major = max(number1, number2)
+character = input("Enter a character: ")
+
+if "0" <= character <= "9" :
+    print("It's a number.")
+elif "A" <= character <= "Z" :
+    print("It's is an uppercase letter.")
+elif "a" <= character <= "z" :
+    print("It's a lowercase letter.")
 else:
-    minor = number4
-    if number1 <= number2 and number1 <= number3:
-        middle1, middle2 = number1, min(number2, number3)
-        major = max(number2, number3)
-    elif number2 <= number1 and number2 <= number3:
-        middle1, middle2 = number2, min(number1, number3)
-        major = max(number1, number3)
-    else:
-        middle1, middle2 = number3, min(number1, number2)
-        major = max(number1, number2)
-
-print(minor, middle1, middle2, major)
+    print("It's neither a letter not a number.")
