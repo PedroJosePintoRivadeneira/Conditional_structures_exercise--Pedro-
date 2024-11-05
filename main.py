5f8a45f4e57d48b06aa9a6140d5cc16e1f3a054c
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com.>
2024-11-04 19:58:40 -0500

    Body mass index

--- main.py
+++ main.py
@@ -1,49 +1,31 @@
-#Los tres lados a, b y c de un triángulo deben satisfacer la desigualdad triangular: cada uno de los 
-# lados no puede ser más largo que la suma de los otros dos.
+#El riesgo de que una persona sufra enfermedades coronarias depende de su edad y su índice de masa corporal:
 
-#Escriba un programa que reciba como entrada los tres lados de un triángulo, e indique:
+# 	edad < 45	edad ≥ 45
+#IMC < 22.0	bajo	medio
+#IMC ≥ 22.0	medio	alto
+#El índice de masa corporal es el cuociente entre el peso del individuo en kilos y el cuadrado de su estatura en metros.
 
-#si acaso el triángulo es inválido; y
-#si no lo es, qué tipo de triángulo es.
+#Escriba un programa que reciba como entrada la estatura, el peso y la edad de una persona, y le entregue su condición 
+# de riesgo.
 
-# Ingrese a: 3.9
-#Ingrese b: 6.0
-#Ingrese c: 1.2
-#No es un triangulo valido.
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
+year = int(input("Enter your age in years: "))
 
-#Ingrese a: 1.9
-#Ingrese b: 2
-#Ingrese c: 2
-#El triangulo es isoceles.
+imc = weight / (height ** 2)
 
-#Ingrese a: 3.0
-#Ingrese b: 5.0
-#Ingrese c: 4.0
-#El triangulo es escaleno.
-# Input for the sides of the triangle
-
-a = float(input("Enter a: "))
-b = float(input("Enter b: "))
-c = float(input("Enter c: "))
-
-if a <= 0:
-    print("It is not a valid triangle.")
-elif b <= 0:
-    print("It is not a valid triangle.")
-elif c <= 0:
-    print("It is not a valid triangle.")
-else:
-    if a + b <= c:
-        print("It is not a valid triangle.")
-    elif a + c <= b:
-        print("It is not a valid triangle.")
-    elif b + c <= a:
-        print("It is not a valid triangle.")
+if year < 45:
+    if imc < 22.0:
+        risk = "low"
     else:
-        if a == b and b == c:
-            print("The triangle is equilateral.")
-        elif a == b or a == c or b == c:
-            print("The triangle is isosceles.")
-        else:
-            print("The triangle is scalene.")
-            
+        risk = "medium"
+else:
+    if imc < 22.0:
+        risk = "medium"
+    else:
+        risk = "high"
+
+print(f"""
+      Your body mass index (BMI) is: {imc}
+      Your risk condition is: {risk}
+""")
